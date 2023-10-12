532b12aaac79fc449d7bc23ad991c32661d69ac6
sleepycatcoding <131554884+sleepycatcoding@users.noreply.github.com>
2023-10-12 18:56:02 +0300

    compiler: Add debug flags for easier debugging without recomp
    Now allows printing out all tokens from the file.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use crate::ast::Visitor;
+use clap::Parser;
+use tracing_subscriber::filter::{EnvFilter, LevelFilter};
 
 mod ast;
 mod codegen;
@@ -6,41 +8,56 @@ mod lexer;
 mod optimizer;
 mod parser;
 
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = None)]
+struct Opt {
+    #[clap(flatten, next_help_heading = "Debug Options")]
+    debug: DebugFlags,
+}
+
+#[derive(Debug, clap::Args)]
+#[group(multiple = false)]
+struct DebugFlags {
+    /// Prints out tokens of specified file, instead of compiling it.
+    #[arg(long)]
+    print_tokens: bool,
+}
+
+impl DebugFlags {
+    /// Is set if any of the debug options are set.
+    fn any_set(&self) -> bool {
+        self.print_tokens
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Opt::parse();
 
-    // let file = std::fs::read_to_string("Test.as").unwrap();
-    // let lexer = lexer::Lexer::new(&file);
-    // let parser = parser::as3::grammar::PackageParser::new();
-    // let ast = parser.parse(lexer).unwrap();
-
-    // println!("{:?}", ast);
-
-    // //let mut optimizer = optimizer::AstOptimizer::default();
-
-    // //let new_ast = optimizer.fold_package(ast);
-
-    // let function = &ast.classes[0].functions[0];
-
-    // println!("Expr: {:?}", function);
-
-    // let mut gen = codegen::CodeGenerator::default();
-
-    // gen.visit_function(function).unwrap();
-
-    // println!("Expr {:#?}", gen);
+    let subscriber = tracing_subscriber::fmt::Subscriber::builder()
+        .with_env_filter(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .finish();
+    // Ignore error if it's already been set
+    let _ = tracing::subscriber::set_global_default(subscriber);
 
     let file = std::fs::read_to_string("bug_612641.abs").unwrap();
     let lexer = lexer::Lexer::<crate::lexer::asm::Token>::new(&file);
+
+    if args.debug.any_set() {
+        if args.debug.print_tokens {
+            for token in lexer {
+                tracing::info!("{:?}", token);
+            }
+        }
+
+        return;
+    }
+
     let parser = parser::asm::grammar::FunctionsParser::new();
-
-    // for token in lexer {
-    //     println!("{:?}", token);
-    // }
-
     let out = parser.parse(lexer).unwrap();
 
     println!("{:#?}", out);
-
-    //println!("{:?}", new_ast);
 }
